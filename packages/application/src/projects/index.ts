d98ed0c6969dda389f459c7cd43af0f7234ad7ea
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-24 16:48:04 -0700

    feat(m04.8): export project revision service

--- packages/application/src/projects/index.ts
+++ packages/application/src/projects/index.ts
@@ -1,4 +1,5 @@
 export * from './project-backup';
 export * from './project-backup-service';
+export * from './project-revisions';
 export * from './project-storage';
 export * from './workspace-preferences';
