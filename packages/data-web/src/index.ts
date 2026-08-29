128d0d7e5a12dadfef701aa251355e7ba7d6964b
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-29 10:51:54 -0700

    feat(M08.2): export internal data repository

--- packages/data-web/src/index.ts
+++ packages/data-web/src/index.ts
@@ -4,6 +4,7 @@ import { packageDescriptor as dep1 } from '@electrocraft/application';
 export * from './browser';
 export * from './browser-workspace-preferences';
 export * from './data-source-repository';
+export * from './internal-data-repository';
 export * from './migration';
 export * from './project-revision-repository';
 export * from './repository';
