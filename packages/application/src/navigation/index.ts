42c7039d36e7566cb289855a4e947ac0cffa724d
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-28 19:50:46 -0700

    feat(M07.6): export route access services

--- packages/application/src/navigation/index.ts
+++ packages/application/src/navigation/index.ts
@@ -1,4 +1,5 @@
 export * from './navigation-action-service';
 export * from './navigation-builder-service';
+export * from './navigation-guard-service';
 export * from './navigation-service';
 export * from './screen-navigation-service';
