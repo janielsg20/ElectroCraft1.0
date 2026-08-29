5382656ec7ced60bc7dbcc31d3132ad5cfb4bc8d
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-28 19:53:38 -0700

    feat(M07.7): export navigation compiler ports

--- packages/application/src/navigation/index.ts
+++ packages/application/src/navigation/index.ts
@@ -1,5 +1,6 @@
 export * from './navigation-action-service';
 export * from './navigation-builder-service';
+export * from './navigation-compiler-service';
 export * from './navigation-guard-service';
 export * from './navigation-service';
 export * from './screen-navigation-service';
