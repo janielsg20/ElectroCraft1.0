b575dae0bb98b41e83415dc48110df6e30850f30
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-29 10:52:44 -0700

    feat(M08.2): export internal data adapter

--- packages/connectors/src/index.ts
+++ packages/connectors/src/index.ts
@@ -5,6 +5,7 @@ import {
   type ElectroCraftDataConnector,
 } from '@electrocraft/application';
 
+export * from './internal-data-source-adapter';
 export { dataSourceConnectorRegistry };
 
 export function registerDataSourceAdapter(adapter: DataSourceAdapter) {
