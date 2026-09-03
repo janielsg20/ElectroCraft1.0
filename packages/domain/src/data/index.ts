d02b740776c2b763569f80f25e6a575116f2e1bc
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-09-03 15:13:26 -0400

    feat(M08.8): export field registry contracts

--- packages/domain/src/data/index.ts
+++ packages/domain/src/data/index.ts
@@ -4,9 +4,14 @@ export * from './graphql';
 export * from './secrets';
 export * from './explorer';
 export * from './connector-extension';
+export * from './field-registry';
 export {
+  electroCraftDataFieldConditionSchema,
+  electroCraftDataFieldOptionSchema,
+  electroCraftDataFieldPermissionsSchema,
   electroCraftDataFieldSchema,
   electroCraftDataFieldTypeSchema,
+  electroCraftDataFieldValidationSchema,
   electroCraftDataModelSchema,
   electroCraftDataSchemaSchema,
   getDataField,
@@ -14,7 +19,11 @@ export {
   validateDataSchemaReferences,
   type DataOwnershipReferenceDiagnostic,
   type ElectroCraftDataField,
+  type ElectroCraftDataFieldCondition,
+  type ElectroCraftDataFieldOption,
+  type ElectroCraftDataFieldPermissions,
   type ElectroCraftDataFieldType,
+  type ElectroCraftDataFieldValidation,
   type ElectroCraftDataModel,
   type ElectroCraftDataSchema,
 } from '../contracts/data-definition';
