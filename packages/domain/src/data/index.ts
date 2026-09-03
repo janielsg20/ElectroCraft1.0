0a663e1db6827a00f493f12319c32489bdea8ac2
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-09-03 14:10:11 -0400

    feat(M08.7): export connector extension contract

--- packages/domain/src/data/index.ts
+++ packages/domain/src/data/index.ts
@@ -3,6 +3,7 @@ export * from './rest';
 export * from './graphql';
 export * from './secrets';
 export * from './explorer';
+export * from './connector-extension';
 export {
   electroCraftDataFieldSchema,
   electroCraftDataFieldTypeSchema,
