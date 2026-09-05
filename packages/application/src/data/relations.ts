3c2bb80fc14f3265ef5227515db1e302073a06a9
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-09-05 18:10:44 -0400

    fix(M08.11): make relational record delete return result

--- packages/application/src/data/relations.ts
+++ packages/application/src/data/relations.ts
@@ -36,5 +36,5 @@ export interface InternalRelationRepository {
     input: InternalRelationEdgeUpdate,
   ): Promise<ElectroRelationEdge>;
   deleteRelationEdge(projectId: string, sourceId: string, relationId: string, edgeId: string): Promise<boolean>;
-  prepareRecordDelete(projectId: string, sourceId: string, modelId: string, recordId: string): Promise<void>;
+  prepareRecordDelete(projectId: string, sourceId: string, modelId: string, recordId: string): Promise<boolean>;
 }
